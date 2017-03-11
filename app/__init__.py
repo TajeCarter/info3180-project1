d7ec05dcb6355b8cdd40bc66252c4bb85b1e3758
Taje Carter <taje.carter@mymona.uwi.edu>
2017-03-11 23:37:55 +0000

    Initial Commit

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "S3cR3+K3y"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:password@localhost/project1"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['UPLOAD_FOLDER'] = "./app/static/uploads"
+db = SQLAlchemy(app)
+
+app.config.from_object(__name__)
+from app import views
